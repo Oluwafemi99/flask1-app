88c361a89b3cf224e7da22c3c7e2b55205bd362b
Oluwafemi Dojumo <47150847-oluwafemidojumo@users.noreply.replit.com>
2025-09-04 07:57:03 +0000

    new_migrate

--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(25), default='User')
 
     def __init__(self, username: str, password: str):
