dc9ed2390433f5f4d2fd58ae829b5ab148bab4b3
Oluwafemi Dojumo <47150847-oluwafemidojumo@users.noreply.replit.com>
2025-09-04 07:43:46 +0000

    new_migrate

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     login_manager.init_app(app)
     csrf.init_app(app)
 
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'auth.login' # type: ignore
 
     from app.auth.routes import auth_bp
     from app.main.routes import main_bp
--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(50), default='User')
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(25), default='User')
 
     def __init__(self, username: str, password: str):
         self.username = username
